b92c2c58d1b4d640aa7b09cf957e0000c37b1587
Max <max@maelicke.net>
2020-06-11 18:22:20 +0200

    Added acknowledgements and link to Github repo

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from datetime import datetime
-from datetime import timedelta  
+from datetime import timedelta
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, PolynomialFeatures, StandardScaler
@@ -75,7 +75,7 @@ ticker_filenames = {}
 for filename in os.listdir('data'):
     if filename.endswith('.csv'):
         ticker_filenames[filename[:-4]] = 'data/' + filename
-        
+
 
 # Load pickel data per ticker, regressor and time window
 # pickle data (machine-learning results) are created with py_scripts/create_static_data.py
@@ -89,9 +89,9 @@ def get_window_pred_data(ticker, regressor, window):
     '''
     with open('data/pickles/' + ticker + '_' + regressor + '_' + str(window) + '.pkl', 'rb') as file:
             window_pred_data = pickle.load(file)
-    
+
     return window_pred_data
-    
+
 
 # Create site layout and graphs
 # Filters
@@ -142,7 +142,6 @@ app.layout = dbc.Container([
             html.Div([
                 html.H1('Stock Price Predictor'),
                 html.H5('DO NOT USE FOR INVESTMENT DECISIONS - for educational purposes only', className='text-muted'),
-                html.H5('created with Dash by Plotly', className='text-muted'),
                 html.Hr()
             ])
         ])
@@ -165,7 +164,7 @@ app.layout = dbc.Container([
                                                     ])
                                         )
                                 ]),
-                                        
+
                         dcc.Tab(label = '5-day window', value = '5', children = [
                                 dbc.Card(
                                         dbc.CardBody([
@@ -205,17 +204,18 @@ app.layout = dbc.Container([
                                                     ])
                                         )
                                 ]),
-                        
+
                         ]),
                 html.Div([
                             html.Hr(),
                             dcc.Markdown('''
-                                         When evaluating the different models **Ridge Regression** performed best.  
-                                         The methodology, model comparison and source code can be accessed on my [Github project](https://github.com/CSSEGISandData/COVID-19).
-                                           
-                                         This project was conducted as Capstone project for my Data Scientist Nanodegree at [Udacity](https://www.udacity.com/).  
-                                         
-                                         If you want to learn more about me, please check out my  
+                                         When evaluating the different models **Ridge Regression** performed best.
+                                         The methodology, model comparison and source code can be accessed on my [Github project](https://github.com/MaxMaelicke/dsnd_capstone_stock_price_predictor).
+
+                                         This project was conducted as Capstone project for my **Data Scientist Nanodegree** at [**Udacity**](https://www.udacity.com/).
+                                         Many thanks to **Plotly / Dash** for their awesome open-source graphing and dashboard libraries (https://plotly.com/) an to **Yahoo Finance** for the stock price data (https://finance.yahoo.com/).
+
+                                         If you want to learn more about me, please check out my
                                          * [Homepage](https://www.maelicke.net)
                                          * [Github](https://github.com/MaxMaelicke)
                                          * [LinkedIn](https://www.linkedin.com/in/max-maelicke-025a35137/)
@@ -257,7 +257,7 @@ app.layout = dbc.Container([
         Input('window-tab', 'value')
         ]
         )
-    
+
 # Graph Generation
 def update_graph(stock_dropdown, regressor_dropdown, on, window):
     '''
@@ -273,7 +273,7 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
     # Load & prepare data for graphing
     # window_pred_data columns = y_pred, y_test, y_pred_last, date_arr
     window_pred_data = get_window_pred_data(stock_dropdown, regressor_dropdown, window)
-    
+
     # y_pred array structure is different for three regressors
     if regressor_dropdown in ['Support Vector Regression', 'LASSO Regression', 'Elastic Net Regression']:
         y_pred = window_pred_data[0].tolist()
@@ -281,10 +281,10 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
     else:
         y_pred = [element[0] for element in window_pred_data[0].tolist()]
         y_pred_last = window_pred_data[2].tolist()[0][0]
-    
+
     y_test = [element[0] for element in window_pred_data[1].tolist()]
     x_dates = [str(element) for element in window_pred_data[3][0]]
-    
+
     # Calculate date for future prediction (window is only working days)
     if int(window) > 1:
         window_delta = int(window) * 2
@@ -293,19 +293,19 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
         window_delta = int(window)
     future_pred_date = datetime.strptime(x_dates[-1][:10], '%Y-%m-%d') + timedelta(days=window_delta)
     future_pred_date = datetime.date(future_pred_date)
-    
+
     zip_err = zip(y_pred, y_test)
     err = [pred - test for pred, test in zip_err]
-    
+
     zip_err_p = zip(err, y_test)
     err_perc = [err / test for err, test in zip_err_p]
-    
+
     #err_perc = err / y_test
     err_mean = sum(err) / len(err)
     err_min = min(err)
     err_max = max(err)
     err_5_range = np.count_nonzero((np.array(err_perc) <= 0.05) & (np.array(err_perc) >= -0.05)) / len(err_perc)
-    
+
     # Line Chart
     line_chart = go.Figure()
     line_chart.add_trace(go.Scatter(
@@ -319,13 +319,13 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
             y = y_pred,
             name = 'Predicted',
             mode = 'lines',
-            line = dict(color = 'orange')))        
-           
+            line = dict(color = 'orange')))
+
     line_chart.update_layout(
             autosize = False,
             height = 600,
             width = 800)
-    
+
     # Error switch
     if on == True:
         # Line Chart
@@ -335,7 +335,7 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
             name = 'Error abs.',
             mode = 'lines',
             line = dict(color = 'red')))
-        
+
     # Histogram
     histogram = go.Figure()
     histogram.add_trace(go.Histogram(
@@ -344,7 +344,7 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
     histogram.update_layout(autosize = False,
         height = 600,
         width = 800)
-    
+
     # Card Output
     card_header = str(stock_dropdown) + ' | ' + str(regressor_dropdown)
     card_info_header = 'Prediction for ' + str(future_pred_date)
@@ -353,7 +353,7 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
     card_info_3 = 'Min Error = ' + '{:.2f}'.format(err_min)
     card_info_4 = 'Max Error = ' + '{:.2f}'.format(err_max)
     card_info_5 = 'Errors within +/- 5% = ' + '{:.2%}'.format(err_5_range)
-    
+
     pred_card = dbc.CardBody([
             html.H5(card_info_header, className = 'card-title'),
             html.P(card_info_1, className = 'card-text'),
@@ -363,7 +363,7 @@ def update_graph(stock_dropdown, regressor_dropdown, on, window):
             html.P(card_info_4, className = 'card-text'),
             html.P(card_info_5, className = 'card-text')
             ])
-    
+
     # Output    1-day window                              5-day window                                                10-day window                                  20-day window
     return card_header, line_chart, pred_card, histogram, card_header, line_chart, pred_card, histogram, card_header, line_chart, pred_card, histogram, card_header, line_chart, pred_card, histogram
 
